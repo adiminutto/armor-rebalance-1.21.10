00e4d3198dd3cad53175f40ed15729e609a52f0a
Andrew Diminutto <andrew.diminutto@gmail.com>
2025-12-04 20:07:49 -0500

    Initial commit

---
+++ src/main/java/drewd/armorrebalance/ArmorRebalance.java
@@ -0,0 +1,16 @@
+package drewd.armorrebalance;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ArmorRebalance implements ModInitializer {
+	public static final String MOD_ID = "armor-rebalance";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/drewd/armorrebalance/ArmorRebalanceClient.java
@@ -0,0 +1,9 @@
+package drewd.armorrebalance;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArmorRebalanceClient implements ClientModInitializer {
+    public void onInitializeClient() {
+
+    }
+}
